328c834e8e8eaa4e8ca71ce4751c724499fbc56e
camendoza94 <ca.mendoza968@uniandes.edu.co>
2018-04-01 22:49:27 -0500

    Paso 5 - Test para editar perfil

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -1,12 +1,11 @@
-import random
-import sys
-
 import os
+import sys
 
 __author__ = 'asistente'
 from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
 
 class FunctionalTest(TestCase):
 
@@ -20,7 +19,6 @@ class FunctionalTest(TestCase):
     def test_title(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('Busco Ayuda', self.browser.title)
-
 
     def test_registro(self):
         self.browser.get('http://localhost:8000')
@@ -36,7 +34,8 @@ class FunctionalTest(TestCase):
         experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
         experiencia.send_keys('5')
 
-        self.browser.find_element_by_xpath("//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
+        self.browser.find_element_by_xpath(
+            "//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
         telefono = self.browser.find_element_by_id('id_telefono')
         telefono.send_keys('3166875247')
 
@@ -44,7 +43,7 @@ class FunctionalTest(TestCase):
         correo.send_keys('ca.mendoza968@uniandes.edu.co')
 
         imagen = self.browser.find_element_by_id('id_imagen')
-        ruta= os.path.join(sys.path[0], "polls/files", 'image.jpg')
+        ruta = os.path.join(sys.path[0], "polls/files", 'image.jpg')
         imagen.send_keys(ruta)
 
         nombreUsuario = self.browser.find_element_by_id('id_username')
@@ -56,20 +55,19 @@ class FunctionalTest(TestCase):
         botonGrabar = self.browser.find_element_by_id('id_grabar')
         botonGrabar.click()
         self.browser.implicitly_wait(3)
-        span=self.browser.find_element(By.XPATH, '//span[text()="Camilo Mendoza"]')
+        span = self.browser.find_element(By.XPATH, '//span[text()="Camilo Mendoza"]')
 
         self.assertIn('Camilo Mendoza', span.text)
 
-
     def test_verDetalle(self):
         self.browser.get('http://localhost:8000')
-        span=self.browser.find_element(By.XPATH, '//span[text()="Camilo Mendoza"]')
+        span = self.browser.find_element(By.XPATH, '//span[text()="Camilo Mendoza"]')
         span.click()
 
-        h2=self.browser.find_element(By.XPATH, '//h2[text()="Camilo Mendoza"]')
+        h2 = self.browser.find_element(By.XPATH, '//h2[text()="Camilo Mendoza"]')
 
         self.assertIn('Camilo Mendoza', h2.text)
-    
+
     def test_login(self):
         self.browser.get('http://localhost:8000')
         link = self.browser.find_element_by_id('id_login')
@@ -86,3 +84,49 @@ class FunctionalTest(TestCase):
         span = self.browser.find_element(By.XPATH, '/html/body/ul/li[2]/a')
 
         self.assertIn('Logout', span.text)
+
+    def test_editar(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_login')
+        link.click()
+        nombreUsuario = self.browser.find_element_by_id('id_username')
+        nombreUsuario.send_keys('ca.mendoza968')
+
+        clave = self.browser.find_element_by_id('id_password')
+        clave.send_keys('clave123')
+
+        botonIniciar = self.browser.find_element_by_id('id_login_')
+        botonIniciar.click()
+        self.browser.implicitly_wait(3)
+
+        nombre = self.browser.find_element_by_id('id_nombre')
+        nombre.send_keys('Taidy')
+
+        apellidos = self.browser.find_element_by_id('id_apellidos')
+        apellidos.send_keys('Marrugo')
+
+        experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
+        experiencia.send_keys('10')
+
+        self.browser.find_element_by_xpath(
+            "//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
+        telefono = self.browser.find_element_by_id('id_telefono')
+        telefono.send_keys('0000000000')
+
+        correo = self.browser.find_element_by_id('id_correo')
+        correo.send_keys('taidy@uniandes.edu.co')
+
+        imagen = self.browser.find_element_by_id('id_imagen')
+        ruta = os.path.join(sys.path[0], "polls/files", 'image.jpg')
+        imagen.send_keys(ruta)
+
+        self.browser.find_element_by_id('id_editar').click()
+        self.browser.implicitly_wait(3)
+
+        nombre_2 = self.browser.find_element_by_id('id_nombre')
+        self.assertIn("Taidy", nombre_2.text)
+
+        apellidos_2 = self.browser.find_element_by_id('id_apellidos')
+        self.assertIn("Marrugo", apellidos_2.text)
+
+
