3253174b0b83cb207cdf873529395c8579772a24
tjmarrugo10 <tj.marrugo10@uniandes.edu.co>
2018-04-01 22:09:36 -0500

    paso 4 rojo- se crea tet para login

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -28,27 +28,27 @@ class FunctionalTest(TestCase):
         link.click()
 
         nombre = self.browser.find_element_by_id('id_nombre')
-        nombre.send_keys('Juan Daniel')
+        nombre.send_keys('Camilo')
 
         apellidos = self.browser.find_element_by_id('id_apellidos')
-        apellidos.send_keys('Arevalo')
+        apellidos.send_keys('Mendoza')
 
         experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
         experiencia.send_keys('5')
 
         self.browser.find_element_by_xpath("//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
         telefono = self.browser.find_element_by_id('id_telefono')
-        telefono.send_keys('3173024578')
+        telefono.send_keys('3166875247')
 
         correo = self.browser.find_element_by_id('id_correo')
-        correo.send_keys('jd.patino1@uniandes.edu.co')
+        correo.send_keys('ca.mendoza968@uniandes.edu.co')
 
         imagen = self.browser.find_element_by_id('id_imagen')
         ruta= os.path.join(sys.path[0], "polls/files", 'image.jpg')
         imagen.send_keys(ruta)
 
         nombreUsuario = self.browser.find_element_by_id('id_username')
-        nombreUsuario.send_keys('juan645')
+        nombreUsuario.send_keys('ca.mendoza968')
 
         clave = self.browser.find_element_by_id('id_password')
         clave.send_keys('clave123')
@@ -56,17 +56,33 @@ class FunctionalTest(TestCase):
         botonGrabar = self.browser.find_element_by_id('id_grabar')
         botonGrabar.click()
         self.browser.implicitly_wait(3)
-        span=self.browser.find_element(By.XPATH, '//span[text()="Juan Daniel Arevalo"]')
+        span=self.browser.find_element(By.XPATH, '//span[text()="Camilo Mendoza"]')
 
-        self.assertIn('Juan Daniel Arevalo', span.text)
+        self.assertIn('Camilo Mendoza', span.text)
 
 
     def test_verDetalle(self):
         self.browser.get('http://localhost:8000')
-        span=self.browser.find_element(By.XPATH, '//span[text()="Juan Daniel Arevalo"]')
+        span=self.browser.find_element(By.XPATH, '//span[text()="Camilo Mendoza"]')
         span.click()
 
-        h2=self.browser.find_element(By.XPATH, '//h2[text()="Juan Daniel Arevalo"]')
+        h2=self.browser.find_element(By.XPATH, '//h2[text()="Camilo Mendoza"]')
 
-        self.assertIn('Juan Daniel Arevalo', h2.text)
+        self.assertIn('Camilo Mendoza', h2.text)
     
+    def test_login(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_login')
+        link.click()
+        nombreUsuario = self.browser.find_element_by_id('id_username')
+        nombreUsuario.send_keys('ca.mendoza968')
+
+        clave = self.browser.find_element_by_id('id_password')
+        clave.send_keys('clave123')
+
+        botonIniciar = self.browser.find_element_by_id('id_login_')
+        botonIniciar.click()
+        self.browser.implicitly_wait(3)
+        span = self.browser.find_element(By.XPATH, '/html/body/ul/li[2]/a')
+
+        self.assertIn('Logout', span.text)
